2647c82938cc8ab9b8f184f80229d8fa418cc6b2
Shakur <shakur.hassan@autolle.com>
2022-01-03 19:47:08 +0200

    create template component

---
+++ src/templates/project-details.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+function projectDetails(props) {
+  return (
+    <Layout>
+      <main className="container">
+        <h1>title</h1>
+        <h3>stack</h3>
+        <div className="mx-auto" style={{ maxWidth: "340px" }}></div>
+        <div></div>
+      </main>
+    </Layout>
+  )
+}
+
+export default projectDetails
