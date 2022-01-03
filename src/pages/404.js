78ef792f89359cbb45bd43881a6bac9ba1e38862
Shakur <shakur.hassan@autolle.com>
2022-01-03 12:38:25 +0200

    add 404 and new page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function NotFound(props) {
+  return (
+    <div>
+      <h1>Sorry the page does not exist</h1>
+      <a href="/">Go to Homepage</a>
+    </div>
+  )
+}
+
+export default NotFound
